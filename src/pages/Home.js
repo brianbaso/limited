8cdbed96cde32ab374ad9165793586286f2f6c60
brianbaso <brianbaso@gmail.com>
2023-06-18 23:32:21 -0500

    last copy change for now. where does the time go?

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,11 @@ function Home() {
         <>
           <div>
             <h1 className="floating-heading">limited 🥀</h1>
-            <p className="home-subheading">Use your DAW with only 4 predetermined tracks allowed.<br/> 20-30 minute sessions. No expectations. Unlimited creativity.</p>
-            <p className="home-body">Limited is an app that pushes music producers into new ways of thinking by putting limits on what they can create. <br/>Tracks made with <i>limited</i> enable you to get unique repetitions in, the best way to practice and get better as a producer.</p>
+            <p className="home-subheading">Unleash your creativity using just 4 predetermined tracks in your DAW.<br/> 20-30 minute sessions. No expectations, just delibarate practice.</p>
+            <p className="home-body">Limited is an app that encourages music producers to explore new creative approaches by imposing restrictions on their creative process. Unlock new facets of your unique style through a proven method that drives rapid improvement as a music producer.</p>
             <div className="home-quote-container">
               <img className="home-quote-photo" src={pharrell} alt="Pharrell Williams" />
-              <p className="home-quote">"Once you see 10,000 buttons and infinite possibilities it just becomes so much you lose yourself. You should start with something that doesn’t accommodate you fully so you strive for more." — Pharrell Williams</p>
+              <p className="home-quote">"Once you see 10,000 buttons and infinite possibilities, it just becomes so much you can lose yourself. You should start with something that doesn’t accommodate you fully so you strive for more." — Pharrell Williams</p>
             </div>
             <div className="home-button-container">
               <Link to="/produce">
