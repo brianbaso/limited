47ddd727c1d2df78173d4f8b19d3e10209ca97b3
“brianbaso” <brianbaso@gmail.com>
2023-07-11 22:00:30 -0500

    track list works with categories

--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -6,50 +6,72 @@ import { MyContext } from '../MyContext';
    - Only one bass
    - Only one piano
 */
-const DRUM_OPTIONS = [
-    "Drums With Clap",
-    "Drums With Snare",
-    "Drums No Hihats",
-    "Drums With Ride",
-    "Stock DAW Drums"
-];
 
-function TrackList() {
-  const [tracks, setTracks] = useState([]);
-  const { cacheSounds } = useContext(MyContext);
-  const allSounds = cacheSounds.concat(DRUM_OPTIONS);
-
-  useEffect(() => {
-    generateTracks();
-  }, []);
-
-  const generateTracks = () => {
-    const selectedTracks = [];
-
-    while (selectedTracks.length < 3) {
-      const randomTrack = allSounds[Math.floor(Math.random() * allSounds.length)];
-
-      if (
-        !selectedTracks.includes(randomTrack) &&
-        !selectedTracks.some((track) => track.includes('Drums') && randomTrack.includes('Drums')) &&
-        !selectedTracks.some((track) => track.includes('Bass') && randomTrack.includes('Bass'))
-      ) {
-        selectedTracks.push(randomTrack);
-      }
+/*
+    Make me a component in react that utilizes an object like the following, each value array can be different lengths but the keys will stay the same:
+    {
+        "lead-and-rhythm": ["Piano", "Rhodes", "Bells", "Guitar", "Saxaphone"],
+        "bass": ["808 Bass", "Bass guitar", "Double Bass"],
+        "drums": ["Pad Drums", "Loop Drum Sample", "Live Drums"]
     }
 
-    setTracks(selectedTracks);
+    With this, generate me a random list of two lead-and-rhythm, one bass, and one drums. Make sure that the same lead-and-rhythm is not chosen twice.
+    
+    Create an ordered list with the selected tracks. Also create a button "Generate new tracks" that will randomize new tracks.
+*/
+const DRUM_OPTIONS = [
+    "Any Drums With Clap",
+    "Any Drums With Snare",
+    "Any Drums No Hihats",
+    "Any Drums With Ride",
+    "Any Stock DAW Drums"
+];
+
+const trackList = {
+  "lead-and-rhythm": ["Piano", "Rhodes", "Bells", "Guitar", "Saxophone"],
+  "bass": ["808 Bass", "Bass guitar", "Double Bass"],
+  "drums": ["Pad Drums", "Loop Drum Sample", "Live Drums"]
+};
+
+function TrackList() {
+  const [selectedTracks, setSelectedTracks] = useState([]);
+  const { cacheSounds } = useContext(MyContext);
+  // Add mandatory drum options
+  const drumSounds = cacheSounds["drums"].concat(DRUM_OPTIONS);
+
+  useEffect(() => {
+    generateRandomTracks();
+  }, []);
+
+  const generateRandomTracks = () => {
+    const leadAndRhythmTracks = [...cacheSounds["lead-and-rhythm"]];
+    const selectedLeadAndRhythm = [];
+
+    // Randomly select two lead-and-rhythm tracks
+    while (selectedLeadAndRhythm.length < 2 && leadAndRhythmTracks.length > 0) {
+      const randomIndex = Math.floor(Math.random() * leadAndRhythmTracks.length);
+      const track = leadAndRhythmTracks.splice(randomIndex, 1)[0];
+      selectedLeadAndRhythm.push(track);
+    }
+
+    // Randomly select one bass track
+    const selectedBass = cacheSounds["bass"][Math.floor(Math.random() * cacheSounds["bass"].length)];
+
+    // Randomly select one drums track
+    const selectedDrums = drumSounds[Math.floor(Math.random() * drumSounds.length)];
+
+    setSelectedTracks([...selectedLeadAndRhythm, selectedBass, selectedDrums]);
   };
 
   return (
     <div>
       <h2>Tracks Allowed</h2>
       <ol>
-        {tracks.map((track, index) => (
+        {selectedTracks.map((track, index) => (
           <li key={index}>{track}</li>
         ))}
       </ol>
-      <button onClick={generateTracks}>Generate New Tracks</button>
+      <button onClick={generateRandomTracks}>Generate New Tracks</button>
     </div>
   );
 }
